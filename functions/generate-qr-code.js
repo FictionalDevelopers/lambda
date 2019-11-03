ed0771cdbfccc319901feb2967cf213735df06f3
Eugene Annienkov <eugene.annienkov@onereach.com>
2019-11-03 19:18:14 +0200

    feature: added layers

---
+++ functions/generate-qr-code.js
@@ -0,0 +1,14 @@
+const QRCode = require('qrcode')
+
+module.exports.handle = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: await QRCode.toDataURL('www.google.com'),
+      },
+      null,
+      2
+    ),
+  };
+};
