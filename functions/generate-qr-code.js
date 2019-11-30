d5d1a341b75bb38e74db847f01462145b06eaf47
Eugene Annenkov <jeka.annenkov@gmail.com>
2019-11-30 17:05:52 +0200

    add network parameters

--- functions/generate-qr-code.js
+++ functions/generate-qr-code.js
@@ -1,11 +1,19 @@
 const QRCode = require('qrcode');
 
 module.exports.handle = async event => {
+  const data = JSON.parse(event.body) || {};
+  const {
+    name,
+    password,
+    protocol
+  } = data;
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: await QRCode.toDataURL('www.google.com'),
+        success: true,
+        url: await QRCode.toDataURL(`WIFI:T:${protocol};S:${name};P:${password};;`),
+        data,
       },
       null,
       2,
